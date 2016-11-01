f59592b31cf9ec295c0dac08ee7a49c972e5a4e9
Matt Swain <m.swain@me.com>
2016-11-01 21:10:58 +0000

    Add ACS reader test

---
+++ tests/test_reader_acs.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+test_reader_acs
+~~~~~~~~~~~~~~~
+
+Test ACS reader.
+
+:copyright: Copyright 2016 by Matt Swain.
+:license: MIT, see LICENSE file for more details.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import io
+import logging
+import os
+import unittest
+
+from chemdataextractor import Document
+from chemdataextractor.reader import AcsHtmlReader
+
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+class TestAcsHtmlReader(unittest.TestCase):
+
+    maxDiff = None
+
+    def test_detect(self):
+        """Test AcsHtmlReader can detect an ACS document."""
+        r = AcsHtmlReader()
+        fname = 'acs.jmedchem.6b00723.html'
+        f = io.open(os.path.join(os.path.dirname(__file__), 'data', 'acs', fname), 'rb')
+        content = f.read()
+        self.assertEqual(r.detect(content, fname=fname), True)
+
+    def test_direct_usage(self):
+        """Test AcsHtmlReader used directly to parse file."""
+        r = AcsHtmlReader()
+        fname = 'acs.jmedchem.6b00723.html'
+        f = io.open(os.path.join(os.path.dirname(__file__), 'data', 'acs', fname), 'rb')
+        content = f.read()
+        d = r.readstring(content)
+        self.assertEqual(len(d.elements), 198)
+
+    def test_document_usage(self):
+        """Test AcsHtmlReader used via Document.from_file."""
+        fname = 'acs.jmedchem.6b00723.html'
+        f = io.open(os.path.join(os.path.dirname(__file__), 'data', 'acs', fname), 'rb')
+        d = Document.from_file(f, readers=[AcsHtmlReader()])
+        self.assertEqual(len(d.elements), 198)
+
+
+if __name__ == '__main__':
+    unittest.main()
